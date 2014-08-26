a1c1dbb36990037e398c94a810bd230bf4386e7b
tom3442 <thomas3442@gmail.com>
2014-08-27 01:21:12 +1000

    adding click to move

---
+++ Assets/Scripts/Hero/ClickEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickEffect : MonoBehaviour {
+
+    public float m_fMaxDisplayTime = 0.5f;
+    private float m_Timer = 0.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Initialise();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_Timer -= 1.0f * Time.deltaTime;
+
+        gameObject.transform.localScale = new Vector3(m_Timer, m_Timer, m_Timer);
+
+        if (m_Timer <= 0.0f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+
+    public void Initialise()
+    {
+        m_Timer = m_fMaxDisplayTime;
+        gameObject.transform.localScale = new Vector3(m_Timer, m_Timer, m_Timer);
+    }
+
+}
---
+++ Assets/Scripts/Hero/HeroScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroScript : MonoBehaviour {
+
+    private NavMeshAgent m_AgentHandle = null;
+    public GameObject m_ClickEffectPREFAB = null;
+    private float m_AttackRangeMaxDist = 2.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_AgentHandle = GetComponentInChildren<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag == "PROMPT")
+                {
+                    m_AgentHandle.SetDestination(hit.collider.gameObject.transform.position);
+                    m_AgentHandle.stoppingDistance = m_AttackRangeMaxDist;
+                }
+                else 
+                {
+                    m_AgentHandle.stoppingDistance = 0.0f;
+
+                    m_AgentHandle.SetDestination(hit.point);
+
+                    GameObject a_ClickEffect = Instantiate(m_ClickEffectPREFAB) as GameObject;
+                    a_ClickEffect.transform.position = hit.point;
+                }
+            }
+        }
+	}
+}
